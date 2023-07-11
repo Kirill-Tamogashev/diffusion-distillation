4d5b982153b6e568820c8444ca6b00d5eed272dd
Kirill-Tamogashev <tamogachev@me.com>
2023-07-11 18:19:23 +0300

    fix: add default log path

--- train_with_stylegan/train.py
+++ train_with_stylegan/train.py
@@ -12,16 +12,17 @@ from train_with_stylegan.utils import configure_unet_model_from_pretrained
 
 
 TEACHER_MODEL_OPTIONS = ["google/ddpm-cifar10-32"]
+DEFAULT_CKPT_DIR = Path("wandb-checkpoints")
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("-n", "--name", type=str, required=True)
     parser.add_argument("-t", "--teacher", type=str, choices=TEACHER_MODEL_OPTIONS)
-    parser.add_argument("-p", "--params", type=Path, required=True, default="./train_with_gan/params/boot.yaml")
-    parser.add_argument("-d", "--device", type=str, required=True)
+    parser.add_argument("-p", "--params", type=Path, default="./train_with_stylegan/params/boot.yaml")
+    parser.add_argument("-d", "--device", type=str, default=0)
     parser.add_argument("--project", type=str, default="diff-to-gan")
-    parser.add_argument("--dir", type=Path, required=True)
+    parser.add_argument("--dir", type=Path, default=DEFAULT_CKPT_DIR)
     parser.add_argument("--use-wandb", action="store_true")
     parser.add_argument("--save-code", action="store_true")
     parser.add_argument("--resume", action="store_true")
